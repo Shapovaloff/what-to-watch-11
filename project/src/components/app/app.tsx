f7121088d73e97e651e60199cb1d70b7e127fe9f
Yury Shapovalov <yurij.shapovalov@in-aim.ru>
2022-11-09 17:34:38 +0300

    3.1. Добавит маршрутизацию.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import AddReview from '../../pages/add-review/add-review';
+import Film from '../../pages/film/film';
 import Main from '../../pages/main/main';
+import MyList from '../../pages/my-list/my-list';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import Player from '../../pages/player/player';
+import SingIn from '../../pages/sign-in/sign-in';
 
 type AppProps = {
   title: string;
@@ -8,11 +16,38 @@ type AppProps = {
 
 function App({title, genre, year}: AppProps): JSX.Element {
   return (
-    <Main
-      title={title}
-      genre={genre}
-      year={year}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main title={title} genre={genre} year={year} />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<Film />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<SingIn />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyList />}
+        />
+        <Route
+          path={AppRoute.Review}
+          element={<AddReview />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
