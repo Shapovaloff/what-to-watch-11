551aa8a404c66f438c697607b2dba6705a2a26c7
Yury Shapovalov <yurij.shapovalov@in-aim.ru>
2022-11-09 15:32:42 +0300

    2.1. Добавит React-компонент главной страницы.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={Setting.title}
+      genre={Setting.genre}
+      year={Setting.year}
+    />
   </React.StrictMode>,
 );
