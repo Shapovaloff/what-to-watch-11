291f83bc8ebbac78626060637da8de4e13f80ce8
Yury Shapovalov <yurij.shapovalov@in-aim.ru>
2022-11-10 14:59:35 +0300

    4.2. Добавит моки комментариев.

---
+++ project/src/types/comments.ts
@@ -0,0 +1,14 @@
+export type User = {
+  id: number;
+  name: string;
+}
+
+export type Comment = {
+  id: number;
+  user: User;
+  rating: number;
+  comment: string;
+  date: string;
+}
+
+export type Comments = Comment[];
