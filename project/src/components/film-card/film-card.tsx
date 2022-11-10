d9547ce2410f871672d3130894df5e85dd3dc4a8
Yury Shapovalov <yurij.shapovalov@in-aim.ru>
2022-11-10 16:37:38 +0300

    4.4. Вынесет films-list в отдельный компонет.

---
+++ project/src/components/film-card/film-card.tsx
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom';
+import {Film} from '../../types/films';
+
+type FilmCardProps = {
+  film: Film;
+}
+
+function FilmCard({film}: FilmCardProps): JSX.Element {
+  const {name, previewImage, id} = film;
+
+  return (
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img src={previewImage} alt={name} width="280" height="175" />
+      </div>
+      <h3 className="small-film-card__title">
+        <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
+      </h3>
+    </article>
+  );
+}
+
+export default FilmCard;
---
+++ project/src/components/films-list/films-list.tsx
@@ -0,0 +1,18 @@
+import {Films} from '../../types/films';
+import FilmCard from '../film-card/film-card';
+
+type FilmsListProps = {
+  films: Films;
+}
+
+function FilmsList({films}: FilmsListProps): JSX.Element {
+  return (
+    <div className="catalog__films-list">
+      {films.map((film) => (
+        <FilmCard key={film.id} film={film} />
+      ))}
+    </div>
+  );
+}
+
+export default FilmsList;
